161d997f62966b3b6e6ada6ab057eb2ace537cc8
JSHWJ <bogum1029@gmail.com>
2023-01-21 05:47:39 +0900

    개발환경세팅

---
+++ vocaAllday/src/main/java/forever/vocaAllday/VocaAlldayApplication.java
@@ -0,0 +1,13 @@
+package forever.vocaAllday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaAlldayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaAlldayApplication.class, args);
+	}
+
+}
