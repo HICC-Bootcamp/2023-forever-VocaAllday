1f6d5290b5e0ed180756a4cef040dd91e591973b
JSHWJ <bogum1029@gmail.com>
2023-01-28 19:08:22 +0900

    Feat #6: 어노테이션 추가

--- vocaAllday/src/main/java/forever/vocaAllday/dto/InputInfoDto.java
+++ vocaAllday/src/main/java/forever/vocaAllday/dto/InputInfoDto.java
@@ -2,9 +2,11 @@ package forever.vocaAllday.dto;
 
 import forever.vocaAllday.entity.InputVoca;
 import forever.vocaAllday.enums.TestType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class InputInfoDto {
     private String[] word;
     private String[] meaning;
@@ -16,5 +18,6 @@ public class InputInfoDto {
         InputVoca inputVoca = new InputVoca(word, meaning);
         return inputVoca;
     }
+
 
 }
