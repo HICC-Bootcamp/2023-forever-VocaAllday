8a7adece4ba68d18b466d5591112377e3e9d6719
Yejee Cho <yejee1018@naver.com>
2023-02-03 00:25:42 +0900

    채점하기 1차

--- vocaAllday/src/main/java/forever/vocaAllday/dto/SentenceFormDto.java
+++ vocaAllday/src/main/java/forever/vocaAllday/dto/SentenceFormDto.java
@@ -10,4 +10,5 @@ public class SentenceFormDto {
     private List<String> wordList;//<보기>에 들어간 단어
     private List<String> meaningList;//뜻
     private List<String> answerList;//정답
+    private String vocaTitle;//단어장 제목
 }
